260a9ea92cc1b063849a6a9c6730637ce8cada1d
dittygoops <aditya.gup16@gmail.com>
2023-04-04 01:07:21 -0700

    Create input functionality

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
 const gridContainer = document.querySelector('.grid-container');
+let cells = null;
 
 function createGrid(num) {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
     for (let i = 0; i < num; i++) {
         const rowContainer = document.createElement('div');
         rowContainer.classList.add('row-container');
@@ -12,13 +17,23 @@ function createGrid(num) {
             rowContainer.appendChild(cell);
         }
     }
+
+    cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.addEventListener('mouseover', addHoverClass));
 }
 
+function getNum() {
+    createGrid(input.value);
+}
 
 function addHoverClass() {
     this.classList.add('hover');
 }
 
 createGrid(16);
-const cells = document.querySelectorAll('.cell');
-cells.forEach(cell => cell.addEventListener('mouseover', addHoverClass))
+
+const input = document.querySelector('.size-input');
+
+
+const btn = document.querySelector('.generate-button');
+btn.addEventListener('click', getNum);
