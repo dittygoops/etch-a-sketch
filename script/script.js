fb29b2ce74db808c20105469f5ea363314e70a5d
dittygoops <aditya.gup16@gmail.com>
2023-04-04 00:27:18 -0700

    Create createGrid() function

--- script/script.js
+++ script/script.js
@@ -1,22 +1,24 @@
 const gridContainer = document.querySelector('.grid-container');
 
-//#region Algorithm to create grid
-for (let i = 0; i < 10; i++) {
-    const rowContainer = document.createElement('div');
-    rowContainer.classList.add('row-container');
-    gridContainer.appendChild(rowContainer);
-
-    for (let j = 0; j < 10; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        rowContainer.appendChild(cell);
+function createGrid(num) {
+    for (let i = 0; i < num; i++) {
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('row-container');
+        gridContainer.appendChild(rowContainer);
+    
+        for (let j = 0; j < num; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            rowContainer.appendChild(cell);
+        }
     }
 }
-//#endregion
+
 
 function addHoverClass() {
     this.classList.add('hover');
 }
 
+createGrid(16);
 const cells = document.querySelectorAll('.cell');
 cells.forEach(cell => cell.addEventListener('mouseover', addHoverClass))
