c822dc2ed36f7edd2cbe7520f5912d4b000868b7
dittygoops <aditya.gup16@gmail.com>
2023-04-03 21:50:08 -0700

    Fix adding classes in script

--- script/script.js
+++ script/script.js
@@ -2,12 +2,12 @@ const gridContainer = document.querySelector('.grid-container');
 
 for (let i = 0; i < 10; i++) {
     const rowContainer = document.createElement('div');
-    rowContainer.class = 'row-container';
+    rowContainer.classList.add('row-container');
     gridContainer.appendChild(rowContainer);
 
     for (let j = 0; j < 10; j++) {
         const cell = document.createElement('div');
-        cell.class = 'cell';
+        cell.classList.add('cell');
         rowContainer.appendChild(cell);
     }
 }
